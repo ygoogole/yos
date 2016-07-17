08ecfb790796dfdefb7e366722bba1231fe916ec
Yi Cao <cao.yi.sjtu@gmail.com>
2016-07-17 22:16:54 +0100

    print str with color

--- init/entry.c
+++ init/entry.c
@@ -8,8 +8,8 @@ int kern_entry()
 {
     console_clear();
 
-    console_write_colr("Hello, OS kernel!\n", rc_black, rc_green);
+    console_write_color("!!Second Time ! Hello, OS kernel!\n", rc_black, rc_green);
 
-	return 0;
+    return 0;
 }
 
