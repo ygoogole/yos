c741fa6a327009cd254199fdd1ddbd72d0495f31
Yi Cao <cao.yi.sjtu@gmail.com>
2016-07-24 17:36:22 +0100

    implement physical memory management

--- init/entry.c
+++ init/entry.c
@@ -3,6 +3,8 @@
  */
 
 #include "console.h"
+#include "debug.h"
+#include "pmm.h"
 
 int kern_entry()
 {
@@ -13,6 +15,26 @@ int kern_entry()
     printk_color(rc_black, rc_green, "Hello, OS kernel!\n");
 
     panic("test");
+
+	printk("kernel in memory start: 0x%08X\n", kern_start);
+	printk("kernel in memory end:   0x%08X\n", kern_end);
+	printk("kernel in memory used:   %d KB\n\n", (kern_end - kern_start) / 1024);
+	
+	show_memory_map();
+	init_pmm();
+
+	printk_color(rc_black, rc_red, "\nThe Count of Physical Memory Page is: %u\n\n", phy_page_count);
+
+	uint32_t allc_addr = NULL;
+	printk_color(rc_black, rc_light_brown, "Test Physical Memory Alloc :\n");
+	allc_addr = pmm_alloc_page();
+	printk_color(rc_black, rc_light_brown, "Alloc Physical Addr: 0x%08X\n", allc_addr);
+	allc_addr = pmm_alloc_page();
+	printk_color(rc_black, rc_light_brown, "Alloc Physical Addr: 0x%08X\n", allc_addr);
+	allc_addr = pmm_alloc_page();
+	printk_color(rc_black, rc_light_brown, "Alloc Physical Addr: 0x%08X\n", allc_addr);
+	allc_addr = pmm_alloc_page();
+	printk_color(rc_black, rc_light_brown, "Alloc Physical Addr: 0x%08X\n", allc_addr);
 
     return 0;
 }
